2882c312d10a5603635b81cb0531f6d78b12aafc
Jactry Zeng <jactry@foxmail.com>
2014-07-28 14:58:39 +0800

    Update to

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+project=`basename \`pwd\``
+cd $project
+git show -s --format=%H &> ../HEAD.txt
+git pull origin master --tags
+git show -s --format=%H &> ../new_HEAD.txt
+cd ..
+if [ `cat new_HEAD.txt` != `cat HEAD.txt` ]; then
+    git add -u
+    git commit -m "Update to $new_HEAD"
+    git push origin master
+fi
+rm new_HEAD.txt
